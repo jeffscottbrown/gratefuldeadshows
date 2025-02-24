d862a58f6bc05c350abef54e2b824ec2c1137d09
Jeff Brown <brownj@objectcomputing.com>
2025-02-23 22:10:36 -0600

    add dynamic quotes

--- server/server.go
+++ server/server.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"html/template"
+	"math/rand"
 	"net/http"
 	"strconv"
 	"time"
@@ -106,9 +107,41 @@ func configureApplicationHandlers(router *gin.Engine) {
 		})
 	})
 
+	router.GET("/quote", func(c *gin.Context) {
+		c.String(http.StatusOK, getRandomGratefulDeadQuote())
+	})
+
 	router.Static("/static", "server/assets")
 }
 
 func formatDate(t time.Time, layout string) string {
 	return t.Format(layout)
+}
+
+func getRandomGratefulDeadQuote() string {
+	rand.Seed(time.Now().UnixNano())
+	return quotes[rand.Intn(len(quotes))]
+}
+
+var quotes = []string{
+	"Once in a while you get shown the light in the strangest of places if you look at it right.",
+	"Sometimes the light's all shining on me, other times I can barely see.",
+	"Without love in the dream, it will never come true.",
+	"Strangers stopping strangers just to shake their hand.",
+	"Nothing left to do but smile, smile, smile.",
+	"Every silver lining's got a touch of grey.",
+	"Wake up to find out that you are the eyes of the world.",
+	"Let there be songs to fill the air.",
+	"Ripple in still water, when there is no pebble tossed, nor wind to blow.",
+	"Once in a while you get shown the light in the strangest of places if you look at it right.",
+	"Sometimes we live no particular way but our own.",
+	"Such a long, long time to be gone, and a short time to be there.",
+	"Shall we go, you and I while we can?",
+	"Fare you well, fare you well, I love you more than words can tell.",
+	"Come hear Uncle John's Band by the riverside.",
+	"Don't tell me this town ain't got no heart.",
+	"Light the song with sense and color, hold away despair.",
+	"Reach out your hand if your cup be empty.",
+	"Let there be songs to fill the air.",
+	"Once in a while you get shown the light in the strangest of places if you look at it right.",
 }
