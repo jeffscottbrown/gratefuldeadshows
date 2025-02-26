c5cd020d7d522ddbf7ef2e78ec4ba5ada6ea69ef
Jeff Brown <brownj@objectcomputing.com>
2025-02-26 01:26:48 -0600

    reorganize some template code

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"log/slog"
+
 	"github.com/jeffscottbrown/gratefulweb/db"
 	"github.com/jeffscottbrown/gratefulweb/server"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		slog.Debug("Failed to load .env file", "error", err)
+	}
+
 	db.PrintStatistics()
 	server.Run()
 }
